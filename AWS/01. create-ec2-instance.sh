94752b2b91f1e8671b63c0c1eb2a12ef7f31b704
Girish Mallula <chandramgc@gmail.com>
2020-10-27 05:43:13 -0700

    aws code

--- AWS/01. create-ec2-instance.sh
+++ AWS/01. create-ec2-instance.sh
@@ -27,77 +27,85 @@ CIDR Block - Available Hosts per Subnet
 # Aws reserves first 3 available host IP address
 1. VPC Router
 2. AWS DNS
-3. Future Use
+3. Future Values
 
 # Create a VPC
-aws ec2 create-vpc \
-  --cidr-block 10.32.0.0/20 \
+aws ec2 create-vpc --cidr-block 10.32.0.0/20  --region us-east-1 \
   --query 'Vpc.{VpcId: VpcId}'
 
 {
-  "VpcId": "vpc-0ba44bd3cd15e5616"
+  "VpcId": "vpc-09a48abc6d6cce1cf"
 }
 
 # Create a Subnet
 aws ec2 create-subnet \
-  --vpc-id vpc-0ba44bd3cd15e5616 \
+  --vpc-id vpc-09a48abc6d6cce1cf \
   --cidr-block 10.32.0.1/24 \
+  --region us-east-1 \
   --query 'Subnet.{SubnetId: SubnetId}'
 
 {
-    "SubnetId": "subnet-016fafedc73e110f9"
+    "SubnetId": "subnet-00c8f6662b5bbf0d0"
 }
 
 
 # Create a Internet-gateway
 aws ec2 create-internet-gateway \
+  --region us-east-1 \
   --query 'InternetGateway.{InternetGatewayId: InternetGatewayId}'
 
 {
-    "InternetGatewayId": "igw-0161e92e36607065c"
+    "InternetGatewayId": "igw-0f1a90ae375542231"
 }
 
 
 # Attach internet gateway
 aws ec2 attach-internet-gateway \
-  --vpc-id vpc-0ba44bd3cd15e5616 \
-  --internet-gateway-id igw-0161e92e36607065c
+  --vpc-id vpc-09a48abc6d6cce1cf \
+  --region us-east-1 \
+  --internet-gateway-id igw-0f1a90ae375542231
 
 # Create Route Table
 aws ec2 create-route-table \
-  --vpc-id vpc-0ba44bd3cd15e5616 \
+  --vpc-id vpc-09a48abc6d6cce1cf \
+  --region us-east-1 \
   --query 'RouteTable.{RouteTableId: RouteTableId}'
 
 {
-    "RouteTableId": "rtb-094afd8182407e9b8"
+    "RouteTableId": "rtb-0d488e482c2f42666"
 }
 
 
 # Create Route
 aws ec2 create-route \
-  --route-table-id rtb-094afd8182407e9b8 \
+  --route-table-id rtb-0d488e482c2f42666 \
   --destination-cidr-block 0.0.0.0/0 \
-  --gateway-id igw-0161e92e36607065c
+  --region us-east-1 \
+  --gateway-id igw-0f1a90ae375542231
 
 aws ec2 describe-subnets \
   --filters "Name=vpc-id,Values=vpc-2f09a348" \
+  --region us-east-1 \
   --query 'Subnets[*].{ID:SubnetId,CIDR:CidrBlock}'
 
 # Associate route table to subnet
 aws ec2 associate-route-table \
-  --subnet-id subnet-016fafedc73e110f9 \
-  --route-table-id rtb-094afd8182407e9b8
+  --subnet-id subnet-00c8f6662b5bbf0d0 \
+  --region us-east-1 \
+  --route-table-id rtb-0d488e482c2f42666
 
 # Enable public ip for your Subnet
 aws ec2 modify-subnet-attribute \
-  --subnet-id subnet-016fafedc73e110f9 \
+  --subnet-id subnet-00c8f6662b5bbf0d0 \
+  --region us-east-1 \
   --map-public-ip-on-launch
 
 # Create Key Pair
 aws ec2 create-key-pair \
-  --key-name GirishKeyPair \
+  --key-name GirishKeyPairNew \
   --query 'KeyMaterial' \
-  --output text > GirishKeyPair.pem
+  --region us-east-1 \
+  --output text > GirishKeyPairNew.pem
 
 chmod 400 GirishKeyPair.pem
 
@@ -105,27 +113,30 @@ chmod 400 GirishKeyPair.pem
 aws ec2 create-security-group \
   --group-name SSHAccess \
   --description "Security group for SSH access" \
-  --vpc-id vpc-0ba44bd3cd15e5616
+  --region us-east-1 \
+  --vpc-id vpc-09a48abc6d6cce1cf
 
 {
-    "GroupId": "sg-0352956280273ee96"
+    "GroupId": "sg-086fddbcbcf989c68"
 }
 
 # Create rules in Security Group
 aws ec2 authorize-security-group-ingress \
-  --group-id sg-0352956280273ee96 \
+  --group-id sg-086fddbcbcf989c68 \
   --protocol tcp \
   --port 22 \
-  --cidr 68.2.246.213/32
+  --cidr 68.2.246.213/32 \
+  --region us-east-1
 
 # Create ec2 instance
 aws ec2 run-instances \
   --image-id ami-a4827dc9 \
   --count 1 \
   --instance-type t2.micro \
-  --key-name GirishKeyPair \
-  --security-group-ids sg-0352956280273ee96 \
-  --subnet-id subnet-016fafedc73e110f9
+  --key-name GirishKeyPairNew \
+  --security-group-ids sg-086fddbcbcf989c68 \
+  --subnet-id subnet-00c8f6662b5bbf0d0 \
+  --region us-east-1
 
 {
     "Groups": [],
@@ -154,8 +165,8 @@ aws ec2 run-instances \
                 "Name": "pending"
             },
             "StateTransitionReason": "",
-            "SubnetId": "subnet-016fafedc73e110f9",
-            "VpcId": "vpc-0ba44bd3cd15e5616",
+            "SubnetId": "subnet-00c8f6662b5bbf0d0",
+            "VpcId": "vpc-09a48abc6d6cce1cf",
             "Architecture": "x86_64",
             "BlockDeviceMappings": [],
             "ClientToken": "",
@@ -174,7 +185,7 @@ aws ec2 run-instances \
                     "Groups": [
                         {
                             "GroupName": "SSHAccess",
-                            "GroupId": "sg-0352956280273ee96"
+                            "GroupId": "sg-086fddbcbcf989c68"
                         }
                     ],
                     "Ipv6Addresses": [],
@@ -190,8 +201,8 @@ aws ec2 run-instances \
                     ],
                     "SourceDestCheck": true,
                     "Status": "in-use",
-                    "SubnetId": "subnet-016fafedc73e110f9",
-                    "VpcId": "vpc-0ba44bd3cd15e5616",
+                    "SubnetId": "subnet-00c8f6662b5bbf0d0",
+                    "VpcId": "vpc-09a48abc6d6cce1cf",
                     "InterfaceType": "interface"
                 }
             ],
@@ -200,7 +211,7 @@ aws ec2 run-instances \
             "SecurityGroups": [
                 {
                     "GroupName": "SSHAccess",
-                    "GroupId": "sg-0352956280273ee96"
+                    "GroupId": "sg-086fddbcbcf989c68"
                 }
             ],
             "SourceDestCheck": true,
@@ -229,7 +240,7 @@ aws ec2 run-instances \
 }
 
 
-
+aws ec2 describe-instances --instance-ids i-0941482217ef0fb61
 
 {
     "Reservations": [
@@ -261,8 +272,8 @@ aws ec2 run-instances \
                         "Name": "running"
                     },
                     "StateTransitionReason": "",
-                    "SubnetId": "subnet-016fafedc73e110f9",
-                    "VpcId": "vpc-0ba44bd3cd15e5616",
+                    "SubnetId": "subnet-00c8f6662b5bbf0d0",
+                    "VpcId": "vpc-09a48abc6d6cce1cf",
                     "Architecture": "x86_64",
                     "BlockDeviceMappings": [
                         {
@@ -296,7 +307,7 @@ aws ec2 run-instances \
                             "Groups": [
                                 {
                                     "GroupName": "SSHAccess",
-                                    "GroupId": "sg-0352956280273ee96"
+                                    "GroupId": "sg-086fddbcbcf989c68"
                                 }
                             ],
                             "Ipv6Addresses": [],
@@ -317,8 +328,8 @@ aws ec2 run-instances \
                             ],
                             "SourceDestCheck": true,
                             "Status": "in-use",
-                            "SubnetId": "subnet-016fafedc73e110f9",
-                            "VpcId": "vpc-0ba44bd3cd15e5616",
+                            "SubnetId": "subnet-00c8f6662b5bbf0d0",
+                            "VpcId": "vpc-09a48abc6d6cce1cf",
                             "InterfaceType": "interface"
                         }
                     ],
@@ -327,7 +338,7 @@ aws ec2 run-instances \
                     "SecurityGroups": [
                         {
                             "GroupName": "SSHAccess",
-                            "GroupId": "sg-0352956280273ee96"
+                            "GroupId": "sg-086fddbcbcf989c68"
                         }
                     ],
                     "SourceDestCheck": true,
@@ -356,26 +367,26 @@ aws ec2 run-instances \
     ]
 }
 
-ssh -i "GirishKeyPair.pem" ec2-user@34.239.158.29
+ssh -i "GirishKeyPairNew.pem" ec2-user@52.207.105.248
 
 
 # Delete your ec2
-aws ec2 terminate-instances --instance-ids i-0fd46c948d9d6e0a6
+aws ec2 terminate-instances --instance-ids i-0d7ea3bef84d80ac7 --region us-east-1
 
 # Delete your security group
-aws ec2 delete-security-group --group-id sg-0352956280273ee96
+aws ec2 delete-security-group --group-id sg-086fddbcbcf989c68
 
 # Delete your subnets
-aws ec2 delete-subnet --subnet-id subnet-016fafedc73e110f9
+aws ec2 delete-subnet --subnet-id subnet-00c8f6662b5bbf0d0
 
 # Delete your custom route table
-aws ec2 delete-route-table --route-table-id rtb-094afd8182407e9b8
+aws ec2 delete-route-table --route-table-id rtb-0d488e482c2f42666
 
 # Detach your Internet gateway from your VPC
-aws ec2 detach-internet-gateway --internet-gateway-id igw-0161e92e36607065c --vpc-id vpc-0ba44bd3cd15e5616
+aws ec2 detach-internet-gateway --internet-gateway-id igw-0f1a90ae375542231 --vpc-id vpc-09a48abc6d6cce1cf
 
 # Delete your Internet gateway
-aws ec2 delete-internet-gateway --internet-gateway-id igw-0161e92e36607065c
+aws ec2 delete-internet-gateway --internet-gateway-id igw-0f1a90ae375542231
 
 # Delete your VPC
-aws ec2 delete-vpc --vpc-id vpc-0ba44bd3cd15e5616
+aws ec2 delete-vpc --vpc-id vpc-09a48abc6d6cce1cf
